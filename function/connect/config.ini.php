bcd027f0b27519b7328aaf8caa18a8589313c8ad
transang0307of-sys <transang0307of@gmail.com>
2026-07-20 18:47:06 +0700

    Update config.ini.php

--- function/connect/config.ini.php
+++ function/connect/config.ini.php
@@ -1,18 +1,8 @@
 <?php
 $config = [
-    'server'   => 'dpg-d9eqe5b7uimc73a73010-a.oregon-postgres.render.com', // Hoặc dùng 'dpg-d9eqe5b7uimc73a73010-a' nếu chạy chung trên Render
-    'port'     => '5432',
-    'username' => 'billviet_user',
-    'password' => 'Zx0WVFiKfPO1dxgRhy140kyvuHd59tMe',
-    'database' => 'billviet',
+    'server'   => 'tokaido.proxy.rlwy.net',
+    'port'     => '24575',
+    'username' => 'root',
+    'password' => 'bNfBDgkYBcxYPNbsxHIHEbXvzkDIeHIX',
+    'database' => 'railway',
 ];
-
-// Hàm hỗ trợ tạo chuỗi kết nối cho PostgreSQL
-$conn_string = "host={$config['server']} port={$config['port']} dbname={$config['database']} user={$config['username']} password={$config['password']} sslmode=require";
-
-$conn = pg_connect($conn_string);
-
-if (!$conn) {
-    die("Lỗi kết nối đến Render Database!");
-}
-?>
