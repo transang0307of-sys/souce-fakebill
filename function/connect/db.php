df41a0d459f4fcf31bf48820aea3b62d700f9514
transang0307of-sys <transang0307of@gmail.com>
2026-07-20 19:27:03 +0700

    Update database connection details in db.php

--- function/connect/db.php
+++ function/connect/db.php
@@ -1,9 +1,9 @@
 <?php
-$localhost_db = 'mysql.railway.internal';
+$localhost_db = 'tokaido.proxy.rlwy.net';
 $username_db  = 'root';
 $password_db  = 'bNfBDgkYBcxYPNbsxHIHEbXvzkDIeHIX';
 $database_db  = 'railway';
-$port_db      = 3306;
+$port_db      = 24575;
 
 $conn = mysqli_connect(
     $localhost_db,
