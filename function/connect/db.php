530678839b68c2a823193f4e1b1ee07ad05ed339
transang0307of-sys <transang0307of@gmail.com>
2026-07-20 18:47:50 +0700

    Update db.php

--- function/connect/db.php
+++ function/connect/db.php
@@ -1,17 +1,20 @@
 <?php
-$localhost_db = 'dpg-d9eqe5b7uimc73a73010-a.oregon-postgres.render.com'; // Hoặc 'dpg-d9eqe5b7uimc73a73010-a' nếu chạy web trực tiếp trên Render
-$username_db  = 'billviet_user';
-$password_db  = 'Zx0WVFiKfPO1dxgRhy140kyvuHd59tMe';
-$database_db  = 'billviet';
-$port_db      = '5432';
+$localhost_db = 'tokaido.proxy.rlwy.net';
+$username_db  = 'root';
+$password_db  = 'bNfBDgkYBcxYPNbsxHIHEbXvzkDIeHIX';
+$database_db  = 'railway';
+$port_db      = 24575;
 
-// Chuỗi kết nối đến PostgreSQL trên Render
-$conn_string = "host={$localhost_db} port={$port_db} dbname={$database_db} user={$username_db} password={$password_db} sslmode=require";
-
-// Thực hiện kết nối
-$conn = pg_connect($conn_string);
+$conn = mysqli_connect(
+    $localhost_db,
+    $username_db,
+    $password_db,
+    $database_db,
+    $port_db
+);
 
 if (!$conn) {
-    die("Lỗi kết nối đến Render Database!");
+    die("Lỗi kết nối: " . mysqli_connect_error());
 }
-?>
+
+mysqli_set_charset($conn, "utf8mb4");
