3268139cee746117aeae80f22ccb8112eb4ecc83
transang0307of-sys <transang0307of@gmail.com>
2026-07-20 23:43:31 +0700

    Update dash.php

--- dash.php
+++ dash.php
@@ -3,7 +3,7 @@
 <?php
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-require $_SERVER['DOCUMENT_ROOT'].'/include/head.php';
+require $_SERVER['DOCUMENT_ROOT'].'/include/head.php';?>
 // .
 <main>
     <script>
