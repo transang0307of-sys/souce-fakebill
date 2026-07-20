d57f5ab04e7f5f8274d54a54d81a7e985d5502b0
transang0307of-sys <transang0307of@gmail.com>
2026-07-20 20:21:11 +0700

    Update config.ini.php

--- function/connect/config.ini.php
+++ function/connect/config.ini.php
@@ -1,8 +1,8 @@
-<?php
+p<?php
 $config = [
-    'server'   => 'tokaido.proxy.rlwy.net',
-    'port'     => '24575',
-    'username' => 'root',
-    'password' => 'bNfBDgkYBcxYPNbsxHIHEbXvzkDIeHIX',
-    'database' => 'railway',
+    'server'   => 'mysql-3f4b73e7-transang0307of-e326.d.aivencloud.com',
+    'port'     => '16996',
+    'username' => 'avnadmin',
+    'password' => 'AVNS_J9V2kgQz6u--1f44ZMR',
+    'database' => 'defaultdb',
 ];
